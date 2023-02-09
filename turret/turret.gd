4dd3d9b104ed4f3e84246718ff06dbb177c375cd
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2023-02-09 13:51:25 +0400

    initial commit

---
+++ turret/turret.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+export var player_position = Vector2(0,0)
+
+signal shoot(initial_position, movement_vector)
+
+const bullet_scene = preload("res://bullets/Bullet.tscn")
+
+var RELOAD_TIME = 2
+var pseudo_timer = rand_range(0, RELOAD_TIME)
+
+func _physics_process(delta):
+	look_at(player_position)
+	
+	pseudo_timer += delta
+	if(pseudo_timer > RELOAD_TIME):
+		shoot()
+		pseudo_timer -= RELOAD_TIME
+	
+func shoot():
+	# var bullet = bullet_scene.instance()
+	emit_signal("shoot", position + Vector2(50, 0).rotated(rotation), Vector2(300, 0).rotated(rotation))
+	# bullet.position = position + Vector2(50, 0).rotated(rotation) 
+	# bullet.linear_velocity = Vector2(300, 0).rotated(rotation)
+	# get_parent().add_child(bullet)
+	
